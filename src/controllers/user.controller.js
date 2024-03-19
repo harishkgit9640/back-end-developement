bd108ef2bbce309c44ff902bb63c50db36473b5c
Harish Kumar <harishstudio877@gmail.com>
2024-03-19 07:53:22 +0530

    created subscriber model and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -211,5 +211,72 @@ const incomingRefreshToken = asyncHandler(async (req, res) => {
 })
 
 
+const subscription = asyncHandler(async (req, res) => {
 
-export { registerUser, loginUser, logOutUser, incomingRefreshToken };
+    const userName = await User.findOne(req.param.userName);
+
+    if (!userName?.trim()) {
+        throw new ApiError(401, "UserName not found");
+    }
+
+    const channel = await User.aggregate[
+        {
+            $match: { userName: userName.toLowerCase() },
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localFields: "_id",
+                foreignFields: "channel",
+                as: "subscribers"
+            },
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localFields: "_id",
+                foreignFields: "subscriber",
+                as: "subscribedTo"
+            },
+        },
+        {
+            addFields: {
+                subscriberCount: { $size: "$subscribers" },
+                channelSubscribedToCount: { $size: "$subscribedTo" },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
+            },
+
+        },
+        {
+            $project: {
+                userName: 1,
+                fullName: 1,
+                subscriberCount: 1,
+                channelSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+            }
+        }
+    ]
+
+    if (!channel?.length) {
+        throw new ApiError("channel dose not exist")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, channel[0], "channel fetched successfully"))
+
+
+})
+
+
+export { registerUser, loginUser, logOutUser, incomingRefreshToken, subscription };
